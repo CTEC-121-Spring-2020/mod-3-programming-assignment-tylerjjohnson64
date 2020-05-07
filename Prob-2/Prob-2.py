e534e1fcd136e8b7cd83e2c8947055371de850d4
unknown <t.johnson57@students.clark.edugit config --global user.email t.johnson57@students.clark.edu>
2020-05-07 14:28:59 -0700

    tested and completed

--- Prob-2/Prob-2.py
+++ Prob-2/Prob-2.py
@@ -7,9 +7,39 @@
 '''
 Your IPO comment goes here
 '''
-
+from math import *
 def main():
     # your code goes here
+    wage=0
+    overtimewages=0
+    overtime=0
+    regularwages=0
+    TotalTakeHome=0
+    name=input("Enter your name: ")
+    wage=eval(input("Enter your hourly wage: "))
+    hours=eval(input("How many hours did you work: "))
+    if hours > 40:
+        overtime= (hours-40)
+        hours=40
 
+    overtimewage=(wage*1.5)
+    tax = .20
+    medical = .10
+    regularwages=(wage*hours)
+    overtimewages=(overtime * overtimewage)
+    totalwages= (overtimewages + regularwages)
+    taxwithheld= (totalwages*tax)
+    medicalwithheld=(totalwages*medical)
+    TotalTakeHome=((totalwages-taxwithheld)-medicalwithheld)
+    print("Employee name:",name)
+    print("Hourly Wage:" ,wage)
+    print("Normal Hour Wages:",regularwages)
+    print("Overtime Wages:",overtimewages)
+    print("Total Wages:",totalwages)
+    print("Taxes Withheld:",taxwithheld)
+    print("Medical Withheld:",medicalwithheld)
+    print("Total Take Home:",TotalTakeHome)
+    print()
+    
 
 main()
